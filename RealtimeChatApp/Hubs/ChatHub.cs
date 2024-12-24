7066b58bffceaef73b943f44db06247ae94acca0
phantom99a1 <viendanho99a1@gmail.com>
2024-12-24 21:50:38 +0700

    Add new project

---
+++ RealtimeChatApp/Hubs/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using RealtimeChatApp.Models;
+
+namespace RealtimeChatApp.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task JoinChat(UserConnection connection)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", "admin", $"{connection.UserName} has joined");
+        }
+
+        public async Task JoinSpecificChatRoom(UserConnection connection)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, connection.ChatRoom);
+        }
+    }
+}
---
+++ RealtimeChatApp/Models/UserConnection.cs
@@ -0,0 +1,9 @@
+﻿namespace RealtimeChatApp.Models
+{
+    public class UserConnection
+    {
+        public string UserName { get; set; } = string.Empty;
+
+        public string ChatRoom { get; set; } = string.Empty;
+    }
+}
